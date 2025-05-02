68d38e0ec0270408dd45e159b7d4ce0c09d04179
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-05-02 10:34:37 +0300

    fix(api): 🐛 Update `TargetPagePath` property attributes in `UpdatePageFromHtmlRequest`; Added `DataSource` attribute for improved data handling

--- Apps.AEM/Models/Requests/UpdatePageFromHtmlRequest.cs
+++ Apps.AEM/Models/Requests/UpdatePageFromHtmlRequest.cs
@@ -1,11 +1,13 @@
+using Apps.AEM.Handlers;
 using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 using Blackbird.Applications.Sdk.Common.Files;
 
 namespace Apps.AEM.Models.Requests;
 
 public class UpdatePageFromHtmlRequest
 {
-    [Display("Target page path")]
+    [Display("Target page path"), DataSource(typeof(PageDataHandler))]
     public string TargetPagePath { get; set; } = string.Empty;
     
     public FileReference File { get; set; } = null!;
