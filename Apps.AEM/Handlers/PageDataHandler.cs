ebcb03d799a76797b2feedd4dd6ca91ba659cf8a
Alex Terehov <terales@users.noreply.github.com>
2025-09-18 10:53:54 -0230

    Simplify connection validation

--- Apps.AEM/Handlers/PageDataHandler.cs
+++ Apps.AEM/Handlers/PageDataHandler.cs
@@ -8,17 +8,20 @@ namespace Apps.AEM.Handlers;
 
 public class PageDataHandler(InvocationContext invocationContext) : Invocable(invocationContext), IAsyncDataSourceItemHandler
 {
+    private const int MaxItems = 25;
+
     public async Task<IEnumerable<DataSourceItem>> GetDataAsync(DataSourceContext context, CancellationToken cancellationToken)
     {
         var request = new RestRequest("/content/services/bb-aem-connector/content/events.json");
         request.AddParameter("type", ContentTypes.Page);
+        request.AddParameter("limit", MaxItems);
 
         if (!string.IsNullOrEmpty(context.SearchString))
         {
             request.AddQueryParameter("keyword", context.SearchString);
         }
 
-        var pages = await Client.Paginate<ContentResponse>(request, 100);
+        var pages = await Client.Paginate<ContentResponse>(request, 25);
 
         return pages
             .Where(x => context.SearchString == null || x.Title.Contains(context.SearchString, StringComparison.OrdinalIgnoreCase))
