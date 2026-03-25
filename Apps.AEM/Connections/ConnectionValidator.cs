0092189d9a3a03a36ed83ab8dc36e1418cf17da5
Alex Terehov <terales@users.noreply.github.com>
2026-03-25 06:58:58 -0230

    Rework validator to rely in query builder

--- Apps.AEM/Connections/ConnectionValidator.cs
+++ Apps.AEM/Connections/ConnectionValidator.cs
@@ -1,4 +1,5 @@
 using Apps.AEM.Api;
+using Apps.AEM.Models.ApiPayloads;
 using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Connections;
 using RestSharp;
@@ -15,15 +16,19 @@ public class ConnectionValidator: IConnectionValidator
         {
             var client = new ApiClient(authenticationCredentialsProviders);
 
-            var request = new RestRequest("/content/services/bb-aem-connector/content/events.json");
-            request.AddParameter("limit", 1);
+            var request = new RestRequest("/bin/querybuilder.json")
+                .AddQueryParameter("path", "/content/")
+                .AddQueryParameter("p.guessTotal", "true")
+                .AddQueryParameter("p.limit", "1");
 
-            var response = await client.ExecuteWithErrorHandling(request);
+            var response = await client.ExecuteWithErrorHandling<QueryBuilderDto>(request);
 
             return new()
             {
-                IsValid = response.IsSuccessful,
-                Message = response.IsSuccessful ? "Connection is valid" : $"Connection failed: {response.Content}",
+                IsValid = response.Success,
+                Message = response.Success
+                    ? "Connection is valid"
+                    : "Connection failed: Query Builder response did not return success=true",
             };
         } catch(Exception ex)
         {
